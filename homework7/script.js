0e11dd61b1532d1069a7cadd521ebd0ac507cda2
Ihor Svitlyi <ihor.svilti@gmail.com>
2020-05-10 23:21:15 +0300

    new homework

--- homework7/script.js
+++ homework7/script.js
@@ -7,34 +7,29 @@ const origin = {
 }
 
 const target = {}
-for (const key in origin) {
-    const value = origin[key]
-    console.log(key + ":" + value)
-}
+
 
 
 function copy(target, origin) {
+    for (const key in origin) {
+        const value = origin[key]
+        console.log(key + ":" + value)
+    }
     for (let key of Object.getOwnPropertyNames(origin)) {
         target[key] = origin[key];
     }
 
-
+    if (origin !== target) {
+        console.log("Don not mach")
+    } else {
+        console.log("All is good")
+    }
 }
 copy(target, origin)
-console.log(target)
-
-for (const key in target) {
-    const value = target[key]
-    console.log(key + ":" + value)
-}
 
 
 
-if (origin !== target) {
-    console.log("Don not mach")
-} else {
-    console.log("All is good")
-}
+
 
 
 
@@ -43,26 +38,33 @@ const meUser = {
     firstName: "Ihor",
     lastName: "Svitlyi"
 
-    
+
 }
 const anotherUser = {
     id: 1,
     firstName: "Ihor",
     lastName: "Svitlyi"
-    
-    
+
+
 
 }
-function getCheck(meUser, anotherUser){
-     
-        if( JSON.stringify(meUser) === JSON.stringify(anotherUser)){
-            console.log("Well done")
-        }else{
-            console.log("Not good")
-        }
-    
-}
-    
+function getCheck(meUser, anotherUser) {
 
-getCheck(meUser, anotherUser)
+   if(Object.keys(meUser).length !== Object.keys(anotherUser).length){
+       return false;
+   }for(let propName in meUser){
+       if(! anotherUser.hasOwnProperty(propName)){
+           return false;
+       }
+       if(meUser[propName]!==anotherUser[propName]){
+           return false;
+       }else{
+           return true
+       }
+   }
+
+}
+
+
+console.log(getCheck(meUser, anotherUser))
 
